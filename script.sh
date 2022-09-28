ec7fd899ef9ade235e0c714b07e9b637abf8a9eb
Antonio Jesús Cano Andreu <72340657+aca062@users.noreply.github.com>
2022-09-28 19:08:39 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+/bin/echo "Actualizar repositorios"
+/usr/bin/apt -y update
+/usr/bin/apt -y upgrade
+/bin/echo "Instalar Shell In A Box"
+/usr/bin/apt -y install shellinabox
+/bin/echo "Cambiar password"
+/bin/echo 'ubuntu:ubuntu' | chpasswd
+/bin/echo "Modificar sshd_config"
+/bin/sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+/bin/systemctl restart ssh
