28fbcdd8146de9e8cc1f2317813939f9a58b4c3e
Antonio Jesús Cano Andreu <72340657+aca062@users.noreply.github.com>
2022-10-19 19:52:23 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+/bin/echo "Actualizar repositorios"
+/usr/bin/apt -y update
+/usr/bin/apt -y upgrade
+/bin/echo "Cambiar password"
+/bin/echo 'ubuntu:10151' | chpasswd
+/bin/echo "Modificar sshd_config"
+/bin/sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+/bin/systemctl restart ssh
