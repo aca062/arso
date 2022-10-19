a0ef8ab92188a5afde24c97745140af2faeff7cb
Antonio Jesús Cano Andreu <72340657+aca062@users.noreply.github.com>
2022-10-19 19:55:35 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 /usr/bin/apt -y update
 /usr/bin/apt -y upgrade
 /bin/echo "Cambiar password"
-/bin/echo 'ubuntu:10151' | chpasswd
+/bin/echo 'aca062:10151' | chpasswd
 /bin/echo "Modificar sshd_config"
 /bin/sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 /bin/systemctl restart ssh
