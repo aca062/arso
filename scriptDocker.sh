8fa5a1569c9baacdce28e85e974c650e618907af
Antonio Jesús Cano Andreu <72340657+aca062@users.noreply.github.com>
2022-11-23 18:38:15 +0100

    Create scriptDocker.sh

---
+++ scriptDocker.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+/bin/echo "Actualizar sistema operativo."
+/usr/bin/apt -y update
+/usr/bin/apt -y upgrade
+/bin/echo "Instalar y Docker."
+/usr/bin/apt -y install docker.io
+/usr/bin/apt-get /usr/bin/install ca-certificates curl gnupg lsb-release
+/usr/bin/mkdir -p /etc/apt/keyrings
+/usr/bin/curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+/usr/bin/echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+/usr/bin/chmod a+r /etc/apt/keyrings/docker.gpg
+/usr/bin/apt-get update
+/usr/bin/apt-get /usr/bin/install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+/bin/echo "Cambiar password"
+/bin/echo 'ubuntu:10151' | chpasswd
+/usr/sbin/groupadd docker
+/usr/bin/gpasswd -a ubuntu docker
+/bin/echo "Modificar sshd_config para activar contraseñas"
+/bin/sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+/bin/systemctl restart ssh
